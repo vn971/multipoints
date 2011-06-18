b0cd6e53691aa78895bad63905d93e9d6a5e420d
Alexey Priymak <alxyz2006@yahoo.com>
2011-06-18 20:43:09 +0300

    PointsAI 1.056 deleted

--- src/com/google/sites/priymakpoints/pointsai/pointsAI_1_056/TemplateIO.java
+++
@@ -1,9 +0,0 @@
-package com.google.sites.priymakpoints.pointsai.pointsAI_1_056;
-
-public class TemplateIO{
-	
-protected Template[] getBase(boolean isOffline){
-	return Singleton.getInstance();
-}
-	
-}
